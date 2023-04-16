2a7313a88438e9167130f167b7bfd9fe5e4019ee
Kafle <kaflepk@amazon.com>
2023-04-16 11:24:03 -0400

    hi

--- src/main/java/com/springboot/demo/StudentController.java
+++ src/main/java/com/springboot/demo/StudentController.java
@@ -1,5 +1,8 @@
 package com.springboot.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +11,16 @@ public class StudentController {
 	@GetMapping("/student")
 	public Student getStudent() {
 		return new Student("Patric", "Patel");
+		
+	}
+	@GetMapping("/students")
+	public List<Student> getStudents(){
+		List<Student> students=new ArrayList<>();
+		students.add(new Student("Dipak", "jha"));
+		students.add(new Student("Ram", "Misra"));
+		students.add(new Student("Raj", "kuwar"));
+		students.add(new Student("Nil", "Devish"));
+		return students;
 		
 	}
 	
