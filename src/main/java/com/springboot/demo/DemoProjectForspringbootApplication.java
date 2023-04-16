b886728ef52f0d774b7ca71a4525ab1888b681a3
Puru <purukafle123@gmail.com>
2023-04-16 09:34:25 -0400

    first commit

---
+++ src/main/java/com/springboot/demo/DemoProjectForspringbootApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectForspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectForspringbootApplication.class, args);
+		System.out.println("it is running .........");
+	}
+
+}
